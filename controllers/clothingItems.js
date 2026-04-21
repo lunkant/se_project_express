ee72c83a5816a6902ee410ae0f64e033d23b045d
lunkant <mordecaiambroise38@gmail.com>
2026-04-21 06:45:44 -0400

    fix return issue

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -25,7 +25,7 @@ const createClothingItem = (req, res) => {
     .catch((err) => {
       console.error("Error creating clothing item:", err);
       if (err.name === "ValidationError") {
-        returnres.status(400).send({ message: err.message });
+        return res.status(400).send({ message: err.message });
       }
       return res.status(500).send({ message: err.message });
     });
